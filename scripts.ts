7cfeada14fb82da1960a8dcf8b57478eb1be3902
Kevin Hill <kevinkhill@gmail.com>
2022-05-28 06:56:17 -0700

    updating and removing stuff

--- scripts.ts
+++
@@ -1,9 +0,0 @@
-import { IRadratCli } from '@radrat/cli';
-import { packageScripts } from '../../tools/rr/package';
-
-const scripts = async (cli: IRadratCli) => {
-    const scope = `types-package-json`;
-    await packageScripts(cli, scope);
-};
-
-export default scripts;
